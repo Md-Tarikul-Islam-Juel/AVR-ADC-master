8e92965ea77d2b7739add7cbeebd7e22c029a9b7
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2019-11-24 16:56:10 +0600

    Update AVR_ADC_MASTER_JUEL.h

--- Example/ATmega328p/code/ATmega328p/lib/AVR_ADC_MASTER_JUEL.h
+++ Example/ATmega328p/code/ATmega328p/lib/AVR_ADC_MASTER_JUEL.h
@@ -12,10 +12,20 @@
 
 
 //-----------------This preprocessor ensure before compiling for the right board------------//
-#if defined (__AVR_ATmega32__) || defined (__AVR_ATmega32A__)
-#elif defined (__AVR_ATmega328__) || defined (__AVR_ATmega328P__)
+#if defined(__AVR_ATmega16__) || defined(__AVR_ATmega16A__) || defined(__AVR_ATmega16P__) || defined(__AVR_ATmega16PA__)
+#elif defined(__AVR_ATmega32__) || defined(__AVR_ATmega32A__)
+#elif defined(__AVR_ATmega164__) || defined(__AVR_ATmega164A__) || defined(__AVR_ATmega164P__) || defined(__AVR_ATmega164PA__)
+#elif defined(__AVR_ATmega324__) || defined(__AVR_ATmega324A__) || defined(__AVR_ATmega324P__) || defined(__AVR_ATmega324PA__)
+#elif defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
+#elif defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__)
+#elif defined(__AVR_ATmega8535__)
+#elif defined(__AVR_ATmega8__)
+#elif defined(__AVR_ATmega48__) || defined(__AVR_ATmega48A__) || defined(__AVR_ATmega48P__) || defined(__AVR_ATmega48PA__) || defined(__AVR_ATmega48PB__)
+#elif defined(__AVR_ATmega88__) || defined(__AVR_ATmega88A__) || defined(__AVR_ATmega88P__) || defined(__AVR_ATmega88PA__) || defined(__AVR_ATmega88PB__)
+#elif defined(__AVR_ATmega168__) || defined(__AVR_ATmega168P__) || defined(__AVR_ATmega168A__) || defined(__AVR_ATmega168PA__) || defined(__AVR_ATmega168PB__)
+#elif defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
 #else
-#error "no definitions available for this AVR"
+	#error "no definitions available for this AVR"
 #endif
 //------------------------------------------------------------------------------------------//
 
